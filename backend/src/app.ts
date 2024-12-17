7e3427dffe763941ca808de7e41ed320faa3dc89
foxy667037 <ma667037c@gmail.com>
2024-12-17 11:24:49 +0500

    initial comment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import user from "./routes/user/userRoutes.js";
 import connectToMongo from "./databases/mongodb/mongodb.js";
 import passwordReset from "./routes/passwordReset/passwordReset.js";
 import contactForm from "./routes/contactForm/contactForm.js";
+import userActivity from "./routes/userActivity/userActivity.js";
 
 const app = express();
 
@@ -18,13 +19,15 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/api/getIp", getIp);
-
 app.use("/api/user", user);
 
-app.use("/api/password-reset", passwordReset);
+app.use("/api/getIp", getIp);
 
 app.use("/api/contact-form", contactForm);
+
+app.use("/api/user-activity", userActivity); 
+
+app.use("/api/password-reset", passwordReset);
 
 app.get("/", (req, res) => {
   res.send("backend working!");
